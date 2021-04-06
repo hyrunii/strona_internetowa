7f51a7a1e274b46b9def20438a56b8830c923885
hyru <wiktor.furmanczyk@onet.eu>
2021-04-06 20:56:13 +0200

    Plik zaloguj.php zostal dodany

---
+++ zaloguj.php
@@ -0,0 +1,94 @@
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Osadnicy - załóż darmowe konto!</title>
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	
+	<style>
+		.error
+		{
+			color:red;
+			margin-top: 10px;
+			margin-bottom: 10px;
+		}
+	</style>
+</head>
+
+<body>
+	
+	<form method="post">
+	<div id="container">
+<?php
+
+	session_start();
+	
+	if ((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
+	{
+		header('Location: index.php');
+		exit();
+	}
+
+	require_once "connect.php";
+
+	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+	
+	if ($polaczenie->connect_errno!=0)
+	{
+		echo "Error: ".$polaczenie->connect_errno;
+	}
+	else
+	{
+		$login = $_POST['login'];
+		$haslo = $_POST['haslo'];
+		
+		$login = htmlentities($login, ENT_QUOTES, "UTF-8");
+	
+		if ($rezultat = @$polaczenie->query(
+		sprintf("SELECT * FROM uzytkownicy WHERE user='%s'",
+		mysqli_real_escape_string($polaczenie,$login))))
+		{
+			$ilu_userow = $rezultat->num_rows;
+			if($ilu_userow>0)
+			{
+				$wiersz = $rezultat->fetch_assoc();
+				
+				if (password_verify($haslo, $wiersz['pass']))
+				{
+					$_SESSION['zalogowany'] = true;
+					$_SESSION['id'] = $wiersz['id'];
+					$_SESSION['user'] = $wiersz['user'];
+					
+					$_SESSION['email'] = $wiersz['email'];
+					
+					unset($_SESSION['blad']);
+					$rezultat->free_result();
+					header('Location: gra.php');
+				}
+				else 
+				{
+					$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+					header('Location: index.php');
+				}
+				
+			} else {
+				
+				$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+				header('Location: index.php');
+				
+			}
+			
+		}
+		
+		$polaczenie->close();
+	}
+	
+?>
+<div id="container">
+	</form>
+
+</body>
+</html>
