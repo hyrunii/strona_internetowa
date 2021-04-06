a4dac15ba844e392d80aafc2a783ee3e801eb3ec
hyru <wiktor.furmanczyk@onet.eu>
2021-04-06 20:53:06 +0200

    Plik index.php zostal dodany

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+	session_start();
+	
+	if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
+	{
+		header('Location: gra.php');
+		exit();
+	}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Strona główna</title>
+	<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+	
+	
+	
+	<div id="container">
+	<form action="zaloguj.php" method="post">
+	
+		Login: <br /> <input type="text" name="login" /> <br />
+		Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
+		<input type="submit" value="Zaloguj się" />
+	
+	</form>
+	<form action="rejestracja.php" method="post">
+			
+			<input type="submit" value="Rejestracja">
+	</from>
+	</div>
+	
+<?php
+	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
+?>
+
+</body>
+</html>
